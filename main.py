5ba49e9bce26ae6dd4c89e1d0aa4772f3e87bdee
devwar <108442943+devw4r@users.noreply.github.com>
2023-07-17 21:09:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from pympq.MpqArchive import MpqArchive
 
 if __name__ == '__main__':
-    archive = MpqArchive('/home/user/.wine/drive_c/Program Files (x86)/WoW/Data/interface.MPQ')
-    archive.initialize()
-    for i, entry in enumerate(archive.mpq_entries):
-        try:
-            filename = entry.get_filename().rsplit("\\")[-1]
-            file_bytes = archive.read_file_bytes(entry)
-            if file_bytes:
-                with open(f'{filename}', 'wb') as f:
+    with MpqArchive('/home/user/.wine/drive_c/Program Files (x86)/WoW/Data/interface.MPQ') as archive:
+        for i, entry in enumerate(archive.mpq_entries):
+            try:
+                file_bytes = archive.read_file_bytes(entry)
+                if not file_bytes:
+                    continue
+                with open(f'{entry.filename}', 'wb') as f:
                     f.write(file_bytes)
-        except:
-            print(f'Skipped entry {i}, no filename.')
+            except:
+                print(f'Skipped entry {i}, no filename.')
